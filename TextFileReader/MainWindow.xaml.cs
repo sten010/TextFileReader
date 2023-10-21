52576e04babf1fc54a15ebd6761c674cc7ac7eb3
shishkin sergey <sten-the@yandex.ru>
2023-10-21 13:09:33 +0300

    Добавьте файлы проекта.

---
+++ TextFileReader/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows;
+
+namespace TextFileReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SetText(string filefolder, string fileName)
+        {
+            ListView.Items.Add(filefolder);
+            OpenDialog(filefolder, fileName);
+        }
+        private void OpenDialog(string filefolder, string fileName)
+        {
+            TextReader textReader = new TextReader(filefolder, fileName);
+            textReader.ShowDialog();
+        }
+
+        private string GetFile()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            CommonFileDialogStandardFilters.TextFiles.ShowExtensions = true;
+            CommonFileDialogFilter txtFilter = new CommonFileDialogFilter("Text files", ".txt");
+            txtFilter.ShowExtensions = true;
+            dialog.Filters.Add(txtFilter);
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = false;
+            dialog.Multiselect = false;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                return dialog.FileName;
+            }
+            return string.Empty;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var file = GetFile();
+            if (string.IsNullOrEmpty(file))
+            {
+                MessageBox.Show("Ошибка");
+                return;
+            }
+            var onlyFileName = System.IO.Path.GetFileName(file);
+            SetText(file, onlyFileName);
+        }
+        private void ListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            System.Windows.Controls.ListBox listView = (System.Windows.Controls.ListBox)sender;
+            var onlyFileName = System.IO.Path.GetFileName(listView.SelectedItem.ToString());
+            OpenDialog(listView.SelectedItem.ToString(), onlyFileName);
+        }
+    }
+
+}
---
+++ TextFileReader/TextReader.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace TextFileReader
+{
+    /// <summary>
+    /// Логика взаимодействия для TextReader.xaml
+    /// </summary>
+    public partial class TextReader : Window
+    {
+        public string Folder { get; set; }
+        public TextReader(string folder,string fileName)
+        {
+            InitializeComponent();
+            Folder = folder;
+            this.Title = fileName;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReaderText.Document.Blocks.Clear();
+            using (var sr = new StreamReader(Folder))
+            {
+                ReaderText.Document.Blocks.Add(new Paragraph(new Run(sr.ReadToEnd())));
+            }
+
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SaveText_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange textRange = new TextRange(
+            // TextPointer to the start of content in the RichTextBox.
+             ReaderText.Document.ContentStart,
+            // TextPointer to the end of content in the RichTextBox.
+            ReaderText.Document.ContentEnd
+            );
+            File.WriteAllText(Folder, textRange.Text.Replace("/n","").Trim());
+            Close();
+        }
+    }
+}
